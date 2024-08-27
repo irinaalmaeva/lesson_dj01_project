f4a584bf19843fe4bebb1eac620ee7ef1243b9ae
irinaalmaeva <almaeva.irina@gmail.com>
2024-08-28 00:40:59 +0300

    rem

--- ZeroCoder/ZeroCoder/urls.py
+++ ZeroCoder/ZeroCoder/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('data/', include('data.urls')),
-    path('test/', include('test.urls'))
+    path('data/', include('data.urls'))
+
 ]
