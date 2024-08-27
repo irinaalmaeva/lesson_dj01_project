96f385f8a9f68cc163b8a5978fded9b706fa9428
irinaalmaeva <almaeva.irina@gmail.com>
2024-08-28 00:28:42 +0300

    add code settings.py

--- ZeroCoder/ZeroCoder/urls.py
+++ ZeroCoder/ZeroCoder/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('data/', include('data.urls')),
+    path('test/', include('test.urls'))
 ]
--- ZeroCoder/data/views.py
+++ ZeroCoder/data/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def data(request):
+        return HttpResponse("<h1>Эта страница с данными о моем первом проекте на Django</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Это страница с тестами моего проекта на Django</h1>")
+
